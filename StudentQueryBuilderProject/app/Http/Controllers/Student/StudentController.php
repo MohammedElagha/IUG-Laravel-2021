010be2fe37e2d6ca77f5e9e8b3b0d1865229e3a1
Mohammed Agha <dev.magha@gmail.com>
2021-04-09 11:14:22 +0300

    query builder - where, join, order, group, limit

--- StudentQueryBuilderProject/app/Http/Controllers/Student/StudentController.php
+++ StudentQueryBuilderProject/app/Http/Controllers/Student/StudentController.php
@@ -36,7 +36,7 @@ class StudentController extends Controller
     }
 
     public function edit ($id) {
-        $student = DB::table('students')->where('id', $id)->first();
+        $student = DB::table('students')->where('id', '=', $id)->first();
 
         $nationalities = ["PAL" => "Palestinian" , "SYR" => "Syrian"];
 
@@ -58,3 +58,83 @@ class StudentController extends Controller
         return redirect()->back();
     }
 }
+
+
+
+
+
+/*
+
+select * from students where age >= 18 AND age < 25
+
+$students = DB::table('students')->where('age', '>=', 18)->where('age', '<', 25)->get();
+
+
+##################
+
+select * from students where grade = 'A' OR grade = 'B'
+
+$students = DB::table('students')->where('grade', 'A')->orWhere('grade', 'B')->get();
+
+##################
+
+select * from students where (age >= 18 AND age < 25) AND (grade = 'A' OR grade = 'B')
+
+$min_age = 18;
+$max_age = 25;
+
+$students = DB::table('students')
+    ->where(function ($query) use ($min_age, $max_age) {
+        $query->where('age', '>=', $min_age);
+        $query->where('age', '<', $max_age);
+    })
+    ->where(function ($query) {
+        $query->where('grade', 'A');
+        $query->orWhere('grade', 'B');
+    })
+    
+##################
+
+where('name', 'LIKE', "%$name%")
+
+##################
+
+is null , is not null
+
+whereNull('academic_id')
+whereNotNull('col_name')
+
+##################
+
+$arr = ['A' , 'B' , 'C']
+
+whereIn('students.grade', $arr)
+
+##################
+
+whereRaw('students.grade = grades.final_grade')
+
+
+
+
+############################################################################################################
+
+
+
+employees -> id, name, phone
+cars -> id, name, model, brand, employee_id
+
+employees ... join cars on employees.id = cars.employee_id
+select name as emp_name from employees
+
+$employees = DB::table('employees')->leftJoin('cars', 'employees.id', 'cars.employee_id')
+        ->select('employees.name as emp_name', 'cars.name as car_name')
+        ->orderBy('cars.model', 'DESC')
+        ->orderBy('cars.brand', 'ASC)
+        ->groupBy('employees.id')
+        ->limit(20)
+        ->get();
+
+
+
+*/
